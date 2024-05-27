c41d3bae99dc7418668b6b0cb80dace68db0c163
bob <312777916@qq.com>
2024-05-27 20:48:59 +0800

    add swagger annotations of header

--- anyim-user/src/main/java/com/hibob/anyim/user/controller/UserController.java
+++ anyim-user/src/main/java/com/hibob/anyim/user/controller/UserController.java
@@ -4,6 +4,8 @@ import com.hibob.anyim.common.model.IMHttpResponse;
 import com.hibob.anyim.user.dto.request.*;
 import com.hibob.anyim.user.service.UserService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,7 +26,7 @@ public class UserController {
     /**
      * 账号唯一性校验，场景是用户注册时，输入用户名后，离开焦点时，检查用户名是否已经存在
      * 因此，不应该校验token
-     * TODO 但是，如果没有限制，又有接口被恶意调用，会导致数据库压力过大
+     * TODO 应该不需要这个接口，如果注册的时候账号重复，前端应该按照返回的特定code处理
      * @param dto
      * @return
      */
@@ -34,6 +36,7 @@ public class UserController {
         return userService.validateAccount(dto);
     }
 
+    // TODO 需要验证码机制配合，否则请求容易被ddos攻击
     @ApiOperation(value = "用户注册", notes = "用户注册")
     @PostMapping("/register")
     public ResponseEntity<IMHttpResponse> register(@Valid @RequestBody RegisterReq dto) {
@@ -41,11 +44,21 @@ public class UserController {
     }
 
     @ApiOperation(value = "用户注销", notes = "用户注销")
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "traceId", value = "日志追踪Id", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "timestamp", value = "时间戳", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "sign", value = "请求签名", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "accessToken", value = "accessToken", required = true, paramType = "header", dataType = "String"),
+    })
     @PostMapping("/deregister")
-    public ResponseEntity<IMHttpResponse> deregister(@RequestHeader("accessToken") String accessToken, @Valid @RequestBody DeregisterReq dto) {
+    public ResponseEntity<IMHttpResponse> deregister(@Valid @RequestBody DeregisterReq dto) {
         return userService.deregister(dto);
     }
 
+    // TODO 怎么防ddos攻击？
+    // 前端可以限制登录失败三次，增加图形验证。 需要后端怎么配合？
+    // 应该是要把验证图片的功能单独拆出来当个服务器，验证成功后在把请求转发给登录服务器
+    // 这样，正常登录的请求，直接进登录服务器，不会受到ddos攻击，超过登录失败三次，就会把请求先分流到验证码服务器
     @ApiOperation(value = "用户登录", notes = "用户登录")
     @PostMapping("/login")
     public ResponseEntity<IMHttpResponse> login(@Valid @RequestBody LoginReq dto) {
@@ -53,12 +66,24 @@ public class UserController {
     }
 
     @ApiOperation(value = "用户登出", notes = "用户登出")
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "traceId", value = "日志追踪Id", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "timestamp", value = "时间戳", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "sign", value = "请求签名", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "accessToken", value = "accessToken", required = true, paramType = "header", dataType = "String"),
+    })
     @PostMapping("/logout")
     public ResponseEntity<IMHttpResponse> logout(@Valid @RequestBody LogoutReq dto) {
         return userService.logout(dto);
     }
 
     @ApiOperation(value = "修改密码", notes = "修改密码")
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "traceId", value = "日志追踪Id", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "timestamp", value = "时间戳", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "sign", value = "请求签名", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "accessToken", value = "accessToken", required = true, paramType = "header", dataType = "String"),
+    })
     @PostMapping("/modifyPwd")
     public ResponseEntity<IMHttpResponse> modifyPwd(@Valid @RequestBody ModifyPwdReq dto) {
         return userService.modifyPwd(dto);
@@ -72,30 +97,60 @@ public class UserController {
      * @return
      */
     @ApiOperation(value = "刷新token", notes = "刷新token")
+    @ApiImplicitParams(value = {
+        @ApiImplicitParam(name = "traceId", value = "日志追踪Id", required = true, paramType = "header", dataType = "String"),
+        @ApiImplicitParam(name = "timestamp", value = "时间戳", required = true, paramType = "header", dataType = "String"),
+        @ApiImplicitParam(name = "sign", value = "请求签名", required = true, paramType = "header", dataType = "String"),
+        @ApiImplicitParam(name = "refreshToken", value = "refreshToken", required = true, paramType = "header", dataType = "String"),
+    })
     @PostMapping("/refreshToken")
     public ResponseEntity<IMHttpResponse> refreshToken(@RequestHeader("refreshToken") String refreshToken, @Valid @RequestBody RefreshTokenReq dto) {
         return userService.refreshToken(refreshToken, dto);
     }
 
     @ApiOperation(value = "查询自己信息", notes = "查询自己信息")
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "traceId", value = "日志追踪Id", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "timestamp", value = "时间戳", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "sign", value = "请求签名", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "accessToken", value = "accessToken", required = true, paramType = "header", dataType = "String"),
+    })
     @PostMapping("/querySelf")
     public ResponseEntity<IMHttpResponse> querySelf(@Valid @RequestBody QuerySelfReq dto) {
         return userService.querySelf(dto);
     }
 
     @ApiOperation(value = "修改自己信息", notes = "修改自己信息")
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "traceId", value = "日志追踪Id", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "timestamp", value = "时间戳", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "sign", value = "请求签名", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "accessToken", value = "accessToken", required = true, paramType = "header", dataType = "String"),
+    })
     @PostMapping("/modifySelf")
     public ResponseEntity<IMHttpResponse> modifySelf(@Valid @RequestBody ModifySelfReq dto) {
         return userService.modifySelf(dto);
     }
 
     @ApiOperation(value = "查询别人信息", notes = "查询别人信息")
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "traceId", value = "日志追踪Id", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "timestamp", value = "时间戳", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "sign", value = "请求签名", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "accessToken", value = "accessToken", required = true, paramType = "header", dataType = "String"),
+    })
     @PostMapping("/query")
     public ResponseEntity<IMHttpResponse> query(@Valid @RequestBody QueryReq dto) {
         return userService.query(dto);
     }
 
     @ApiOperation(value = "根据昵称找人", notes = "根据昵称找人")
+    @ApiImplicitParams(value = {
+            @ApiImplicitParam(name = "traceId", value = "日志追踪Id", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "timestamp", value = "时间戳", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "sign", value = "请求签名", required = true, paramType = "header", dataType = "String"),
+            @ApiImplicitParam(name = "accessToken", value = "accessToken", required = true, paramType = "header", dataType = "String"),
+    })
     @PostMapping("/findByNick")
     public ResponseEntity<IMHttpResponse> findByNick(@Valid @RequestBody FindByNickReq dto) {
         return userService.findByNick(dto);
