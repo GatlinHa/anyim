ce66ac7a62462c8ab1f30e33a8ff346f97c21400
bob <312777916@qq.com>
2024-06-14 23:01:36 +0800

    add createGroup rest api in groupmng

--- anyim-groupmng/src/main/java/com/hibob/anyim/groupmng/mybatisplus/EasySqlInjector.java
+++ anyim-groupmng/src/main/java/com/hibob/anyim/groupmng/mybatisplus/EasySqlInjector.java
@@ -13,7 +13,6 @@ public class EasySqlInjector extends DefaultSqlInjector {
     @Override
     public List<AbstractMethod> getMethodList(Configuration configuration, Class<?> mapperClass, TableInfo tableInfo) {
         List<AbstractMethod> methodList = super.getMethodList(configuration, mapperClass, tableInfo);
-        methodList.add(new InsertBatchSomeColumn(i -> i.getFieldFill() != null && i.getFieldFill().equals("INSERT")));
         methodList.add(new InsertBatchSomeColumn());
         return methodList;
     }
