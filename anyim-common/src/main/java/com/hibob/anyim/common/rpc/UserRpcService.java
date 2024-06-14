5bde8555d7fb300ac61e0faafead776e9fb6ee8f
bob <312777916@qq.com>
2024-06-14 15:42:45 +0800

    add queryUserInfo rpc api in user

--- anyim-common/src/main/java/com/hibob/anyim/common/rpc/UserRpcService.java
+++ anyim-common/src/main/java/com/hibob/anyim/common/rpc/UserRpcService.java
@@ -1,7 +1,11 @@
 package com.hibob.anyim.common.rpc;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserRpcService {
     List<String> queryOnline(String account);
+
+    Map<String, Object> queryUserInfo(String account);
+
 }
--- anyim-user/src/main/java/com/hibob/anyim/user/rpc/UserRpcServiceImpl.java
+++ anyim-user/src/main/java/com/hibob/anyim/user/rpc/UserRpcServiceImpl.java
@@ -5,14 +5,19 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.hibob.anyim.common.rpc.UserRpcService;
 import com.hibob.anyim.common.config.JwtProperties;
 import com.hibob.anyim.user.entity.Login;
+import com.hibob.anyim.user.entity.User;
 import com.hibob.anyim.user.mapper.LoginMapper;
+import com.hibob.anyim.user.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.beans.BeanUtils;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @DubboService
@@ -21,10 +26,11 @@ public class UserRpcServiceImpl implements UserRpcService {
 
     private final JwtProperties jwtProperties;
     private final LoginMapper loginMapper;
+    private final UserMapper userMapper;
 
     @Override
     public List<String> queryOnline(String account) {
-        log.info("queryOnline start......");
+        log.info("UserRpcServiceImpl::queryOnline start......");
         LambdaQueryWrapper<Login> queryWrapper = Wrappers.lambdaQuery();
         queryWrapper
                 .select(Login::getUniqueId)
@@ -42,5 +48,18 @@ public class UserRpcServiceImpl implements UserRpcService {
         return list;
     }
 
+    @Override
+    public Map<String, Object> queryUserInfo(String account) {
+        log.info("UserRpcServiceImpl::queryUserInfo start......");
+        LambdaQueryWrapper<User> queryWrapper = Wrappers.lambdaQuery();
+        queryWrapper.eq(User::getAccount, account);
+        List<User> users = userMapper.selectList(queryWrapper);
+        if (users.size() > 0) {
+            Map<String, Object> map = new HashMap<>();
+            BeanUtils.copyProperties(users.get(0), map);
+            return map;
+        }
+        return null;
+    }
 
 }
