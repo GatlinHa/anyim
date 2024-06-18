a624c66cddb6b979af445a5b818292032187200f
bob <312777916@qq.com>
2024-06-18 11:38:50 +0800

    add BaseRequest

--- anyim-common/src/main/java/com/hibob/anyim/common/model/BaseRequest.java
+++ anyim-common/src/main/java/com/hibob/anyim/common/model/BaseRequest.java
@@ -1,24 +1,29 @@
 package com.hibob.anyim.common.model;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.*;
+
 @Data
-@ApiModel("Http短连接请求公共参数")
+@ApiModel("REST短连接请求公共参数")
 public class BaseRequest {
-    //TODO 参数太多，影响测试效率，先注掉
-//    @Max(value = 2, message = "设备类型取值范围:0,2")
-//    @Min(value = 0, message = "设备类型取值范围:0,2")
-//    @ApiModelProperty(value = "设备类型")
-//    private int devType;
-//
-//    @Max(value = 5, message = "客户端类型取值范围:0,5")
-//    @Min(value = 0, message = "客户端类型取值范围:0,5")
-//    @ApiModelProperty(value = "客户端类型")
-//    private String clientVersion;
-//
-//    @Size(max = 10, message = "账号长度不能大于10字符")
-//    @ApiModelProperty(value = "客户端版本")
-//    private String clientVersion;
+
+    @NotNull
+    @Max(value = 10, message = "客户端类型取值范围:0,10")
+    @Min(value = 0, message = "客户端类型取值范围:0,10")
+    @ApiModelProperty(value = "客户端类型")
+    private int clientType;
+
+    @NotEmpty
+    @Size(max = 255, message = "客户端名称长度不能大于255字符")
+    @ApiModelProperty(value = "客户端名称")
+    private String clientName;
+
+    @NotEmpty
+    @Size(max = 255, message = "客户端名称长度不能大于255字符")
+    @ApiModelProperty(value = "客户端版本")
+    private String clientVersion;
 
 }
